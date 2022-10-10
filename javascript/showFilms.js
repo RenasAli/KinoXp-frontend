ffeeef116e674556ff378e302972b6a292c30f89
DanielKoltze <dani64h6@stud.kea.dk>
2022-10-10 21:05:17 +0200

    daniel test branch

--- javascript/showFilms.js
+++ javascript/showFilms.js
@@ -1,22 +1,26 @@
 
 var posters = []
 
-var refreshFilmShowingContainer = () => {
-
-const container = document.querySelector(".filmContainer")
-
-container.replaceChildren()
 
 
-fetchDataByUrl("http://localhost:8080/allFilmShowing").then(data => {
-    
+
+function getAllFilmsByDate(date){
+
+    const container = document.querySelector(".filmContainer")
+
+    container.replaceChildren()
+
+
+fetchDataByUrl("http://localhost:8080/filmShowingsByDate/" + date).then(data => {
+
+    console.log(data)
+   /* for (let i = 0; i < data.length; i++) {
+        
+        
+    }*/
+
 })
 }
 
 
-refreshFilmShowingContainer()
-
-
-fetchDataByUrl("http://localhost:8080/allFilmShowing").then(data => {
-
-})
+getAllFilmsByDate(01-02-2020)
