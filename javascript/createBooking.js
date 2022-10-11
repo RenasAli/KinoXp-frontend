32fb39b7f4435c51fcc2baa87cf84fdff1c852e5
DNY <dannylu1999@gmail.com>
2022-10-11 20:09:35 +0200

    Ticket counter added

--- javascript/createBooking.js
+++ javascript/createBooking.js
@@ -74,6 +74,20 @@ function createBookingPage(data){
     time.innerHTML = data.time
     filmBookingContainer_infoText.appendChild(time)
     
-
-    
 }
+
+
+
+$(document).ready(function(){
+    $('#qty_input').prop('disabled', true);
+    $('#plus-btn').click(function(){
+    	$('#qty_input').val(parseInt($('#qty_input').val()) + 1 );
+    	    });
+        $('#minus-btn').click(function(){
+    	$('#qty_input').val(parseInt($('#qty_input').val()) - 1 );
+    	if ($('#qty_input').val() == 0) {
+			$('#qty_input').val(1);
+		}
+
+    	    });
+ });
