47f328110f6f260b1261f27918cd447c5010f39f
DNY <dannylu1999@gmail.com>
2022-10-12 10:00:03 +0200

    Små rettelser

--- javascript/createBooking.js
+++ javascript/createBooking.js
@@ -77,17 +77,3 @@ function createBookingPage(data){
 }
 
 
-
-$(document).ready(function(){
-    $('#qty_input').prop('disabled', true);
-    $('#plus-btn').click(function(){
-    	$('#qty_input').val(parseInt($('#qty_input').val()) + 1 );
-    	    });
-        $('#minus-btn').click(function(){
-    	$('#qty_input').val(parseInt($('#qty_input').val()) - 1 );
-    	if ($('#qty_input').val() == 0) {
-			$('#qty_input').val(1);
-		}
-
-    	    });
- });
